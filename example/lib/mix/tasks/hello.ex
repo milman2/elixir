4f986ecf172230c1bb9bc8cd383e8386ece3fd1d
milman2 <milman2@voyagergames.gg>
2025-10-14 13:42:57 +0900

    Add Hello module and Mix task for greeting functionality

---
+++ example/lib/mix/tasks/hello.ex
@@ -0,0 +1,13 @@
+defmodule Mix.Tasks.Hello do
+  @moduledoc "The hello mix task: `mix help hello`"
+  use Mix.Task
+
+  @shortdoc "Calls the Hello.say/0 function."
+  def run(_) do
+    # This will start our application
+    Mix.Task.run("app.start")
+
+    # calling our Hello.say() function from earlier
+    Hello.say()
+  end
+end
