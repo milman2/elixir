f207973221b2b92f475883a02d5bf123c0e5f20a
milman2 <milman2@voyagergames.gg>
2025-10-16 17:40:41 +0900

    Update router to include user resources under API scope and comment out the previous users route in the browser scope.

--- REST/forum/lib/forum_web/router.ex
+++ REST/forum/lib/forum_web/router.ex
@@ -18,13 +18,14 @@ defmodule ForumWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-    get "/users", PageController, :users
+    #get "/users", PageController, :users
   end
 
   scope "/api", ForumWeb do
     pipe_through :api
 
     resources "/posts", PostController, except: [:new, :edit]
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Other scopes may use custom stacks.
