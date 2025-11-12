069579f6c0b683d2581b19796415a75ef0af6242
milman2 <milman2@voyagergames.gg>
2025-11-12 11:43:29 +0900

    Enhance Create Gist functionality by adding validation event handling in CreateGistLive, updating form submission in create_gist_live.html.heex, and improving Postgres-CLI documentation with additional commands.

--- LiveView/Postgres-CLI.md
+++ LiveView/Postgres-CLI.md
@@ -1,3 +1,12 @@
-﻿# commands
+﻿# CLI
+```shell
+docker exec -it my-postgres psql -U postgres -d elixir_gist_dev
+SELECT * FROM gists;
+
+
+```
+
+# commands
 - \dt
 - \d table_name
+- \q
--- LiveView/elixir_gist/lib/elixir_gist_web/live/create_gist_live.ex
+++ LiveView/elixir_gist/lib/elixir_gist_web/live/create_gist_live.ex
@@ -9,9 +9,16 @@ defmodule ElixirGistWeb.CreateGistLive do
     {:ok, assign(socket, form: to_form(changeset))}
   end
 
+  def handle_event("validate", %{"gist" => params}, socket) do
+    changeset = Gist.changeset(%Gist{}, params, socket.assigns.current_scope)
+      |> Map.put(:action, :validate)
+    {:noreply, assign(socket, form: to_form(changeset))}
+  end
+
+  # SELECT * FROM gists;
   def handle_event("create", %{"gist" => params}, socket) do
     case Gists.create_gist(socket.assigns.current_scope, params) do
-      {:ok, gist} ->
+      {:ok, _gist} ->
         # 새로운 빈 changeset 생성 (change_gist 대신)
         changeset = Gist.changeset(%Gist{}, %{}, socket.assigns.current_scope)
 
