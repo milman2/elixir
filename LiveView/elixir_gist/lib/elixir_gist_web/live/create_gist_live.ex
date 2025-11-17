1b8e816cae110b1959a9593b42ae07273cc17145
milman2 <milman2@voyagergames.gg>
2025-11-17 14:26:11 +0900

    Refactor CreateGistLive component by extracting form logic into a new GistFormComponent for improved code organization and maintainability. Update HTML structure to utilize the new component, simplifying the CreateGistLive module.

--- LiveView/elixir_gist/lib/elixir_gist_web/live/create_gist_live.ex
+++ LiveView/elixir_gist/lib/elixir_gist_web/live/create_gist_live.ex
@@ -1,38 +1,7 @@
 defmodule ElixirGistWeb.CreateGistLive do
   use ElixirGistWeb, :live_view
 
-  alias ElixirGist.Gists.Gist
-  alias ElixirGist.Gists
-
   def mount(_params, _session, socket) do
-    changeset = Gist.changeset(%Gist{}, %{}, socket.assigns.current_scope)
-    {:ok, assign(socket, form: to_form(changeset))}
-  end
-
-  def handle_event("validate", %{"gist" => params}, socket) do
-    changeset = Gist.changeset(%Gist{}, params, socket.assigns.current_scope)
-      |> Map.put(:action, :validate)
-    {:noreply, assign(socket, form: to_form(changeset))}
-  end
-
-  # SELECT * FROM gists;
-  def handle_event("create", %{"gist" => params}, socket) do
-    case Gists.create_gist(socket.assigns.current_scope, params) do
-      {:ok, gist} ->
-        socket = push_event(socket, "clear-textareas", %{})
-        # 새로운 빈 changeset 생성 (change_gist 대신)
-        changeset = Gist.changeset(%Gist{}, %{}, socket.assigns.current_scope)
-
-        socket =
-          socket
-          |> put_flash(:info, "Gist created successfully!")
-          |> assign(form: to_form(changeset))
-
-        #{:noreply, socket}
-        {:noreply, push_navigate(socket, to: ~p"/gist?#{[id: gist.id]}")}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, form: to_form(changeset))}
-    end
+    {:ok, socket}
   end
 end
---
+++ LiveView/elixir_gist/lib/elixir_gist_web/live/gist_form_component.ex
@@ -0,0 +1,104 @@
+defmodule ElixirGistWeb.GistFormComponent do
+  use ElixirGistWeb, :live_component
+
+  alias ElixirGist.Gists
+  alias ElixirGist.Gists.Gist
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  def update(assigns, socket) do
+    socket = assign(socket, assigns)
+
+    # form이 아직 없으면 새로 생성
+    socket = if Map.has_key?(socket.assigns, :form) do
+      socket
+    else
+      changeset = Gist.changeset(%Gist{}, %{}, socket.assigns.current_scope)
+      assign(socket, form: to_form(changeset))
+    end
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.form for={@form} phx-submit="create" phx-change="validate" phx-target={@myself}>
+        <div class="justify center px-28 w-full space-y-4 mb-10">
+          <.input
+            field={@form[:description]}
+            placeholder="Gist Description..."
+            autocomplete="off"
+            phx-debounce="blur"
+            class="bg-emDark-dark text-white border-white"
+          />
+          <div>
+            <div class="flex p-2 items-center bg-emDark rounded-t-md border border-white">
+              <div class="w-[300px] pl-2">
+                  <.input
+                  field={@form[:name]}
+                  placeholder="Filename including extension..."
+                  autocomplete="off"
+                  phx-debounce="blur"
+                  class="bg-emDark text-emLavender-dark border-0 font-brand font-bold text-base"
+                  />
+              </div>
+            </div>
+            <div id="gist-wrapper" class="flex w-full" phx-update="ignore">
+              <textarea id="line-numbers" class="line-numbers rounded-bl-md" readonly>
+              <%= "1\n" %>
+              </textarea>
+              <div class="flex-1">
+                <.input
+                  id="text-area"
+                  phx-hook="UpdateLineNumbers"
+                  field={@form[:markup_text]}
+                  type="textarea"
+                  class="text-area w-full rounded-br-md font-mono bg-emDark-dark text-white border-white"
+                  placeholder="Insert code..."
+                  spellcheck="false"
+                  autocomplete="off"
+                  rows="20"
+                  phx-debounce="blur"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-end">
+            <.button class="create_button" phx-disable-with="Creating gist...">Create gist</.button>
+          </div>
+        </div>
+      </.form>
+    </div>
+    """
+  end
+
+  def handle_event("validate", %{"gist" => params}, socket) do
+    changeset = Gist.changeset(%Gist{}, params, socket.assigns.current_scope)
+      |> Map.put(:action, :validate)
+    {:noreply, assign(socket, form: to_form(changeset))}
+  end
+
+  # SELECT * FROM gists;
+  def handle_event("create", %{"gist" => params}, socket) do
+    case Gists.create_gist(socket.assigns.current_scope, params) do
+      {:ok, gist} ->
+        socket = push_event(socket, "clear-textareas", %{})
+        # 새로운 빈 changeset 생성 (change_gist 대신)
+        changeset = Gist.changeset(%Gist{}, %{}, socket.assigns.current_scope)
+
+        socket =
+          socket
+          |> put_flash(:info, "Gist created successfully!")
+          |> assign(form: to_form(changeset))
+
+        #{:noreply, socket}
+        {:noreply, push_navigate(socket, to: ~p"/gist?#{[id: gist.id]}")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
+  end
+end
